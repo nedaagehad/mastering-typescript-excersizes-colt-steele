a490a67106c0340cc49aa5ac1fe28d75d2763f6c
Nedaa Gehad <nedaagehd@gmail.co>
2025-01-21 15:20:51 +0200

    Exercise Section 7: Union Types

---
+++ unionsExercise.js
@@ -0,0 +1,34 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create a variable called highScore that can be a number OR a boolean
+var highScore;
+highScore = 50;
+highScore = false;
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// create an array called stuff
+// it can be an array of numbers OR an array of strings
+// it cannot be an array of numbers and strings (mixed together)
+var stuff;
+stuff = ["test", "item"];
+stuff = [12, 15];
+// Create an array called colors that can hold a mixture of RGB and HSL color types
+var colors = [];
+// **********************************************
+// ******************* PART 6 *******************
+// **********************************************
+// Write a function called greet that accepts a single string OR an array of strings
+// It should print "Hello, <name>" for that single person OR greet each person in the array with the same format
+function greet(person) {
+    if (Array.isArray(person)) {
+        for (var i in person) {
+            console.log("Hello, ".concat(person[i]));
+        }
+    }
+    else
+        console.log("Hello, ".concat(person));
+}
+greet("sam");
+greet(["sam", "melanie", "justin"]);
