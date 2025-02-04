cc343e0ff6c16d63066b7d1f35f1b9cacf599d17
Nedaa Gehad <106998660+nedaagehad@users.noreply.github.com>
2025-02-04 19:29:19 +0200

    Delete webpack-ts directory

--- webpack-ts/src/Dog.ts
+++
@@ -1,6 +0,0 @@
-export class Dog {
-  constructor(public name: string, public breed: string, public age: number) {}
-  bark() {
-    console.log("Bark!!")
-  }
-}
--- webpack-ts/src/ShelterDog.ts
+++
@@ -1,7 +0,0 @@
-import { Dog } from "./Dog";
-
-export class ShelterDog extends Dog {
-    constructor( name: string, breed: string, age: number, public shelter: string) {
-        super(name, breed, age)
-    }
-}
